758bfd1988afc4ea8aa03782bb777039325f0170
Blake Fromkin <blakefromkin@gmail.com>
2023-08-06 22:05:53 -0400

    Added tag functionality

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,14 @@
 let contactTemplate = Handlebars.compile($('#contactTemplate').html());
 let contactsTemplate = Handlebars.compile($('#contactsTemplate').html());
 let formTemplate = Handlebars.compile($('#formTemplate').html());
+let tagTemplate = Handlebars.compile($('#tagSelectTemplate').html());
 Handlebars.registerPartial('contactTemplate', $('#contactTemplate').html());
 
-// Fetch contacts to determine initial homepage render
+// Fetch contacts and tags before loading the DOM
 fetchHomepage();
+let addtlTags;
+additionalTags().then(val => addtlTags = val);
+populateTagsFilter();
 
 document.addEventListener("DOMContentLoaded", e => {
   document.getElementById("contacts").addEventListener("click", async e => {
@@ -16,15 +20,13 @@ document.addEventListener("DOMContentLoaded", e => {
       let contactObj = await fetchContact(contactId);
       contactObj.action = "Edit";
       contactObj.contactId = contactId;
-      if (contactObj.tags) {
-        let checkedTags = parseTags(contactObj);
-        checkedTags.forEach(item => contactObj[item] = "checked");
-      }
+      contactObj.additionalTags = addtlTags;
 
       clearNoContactsMsg();
       resetHiddenContactDivs();
       toggleHiddenDivs();
       clearSearchBar();
+      resetTagsFilter();
       let main = document.querySelector("main");
       main.insertAdjacentHTML("afterbegin", formTemplate(contactObj));
       bindFormButtons();      
@@ -63,6 +65,44 @@ document.addEventListener("DOMContentLoaded", e => {
     } else if (!matchExists) {
       noContactsElement.textContent = `There are no contacts starting with ${text}.`;
     }
+
+    if (!text) document.getElementById("tag-select").value = 'all';
+  });
+
+  // Tag filter handling
+  document.getElementById("tag-select").addEventListener("input", e => {
+    let val = e.currentTarget.value;
+    if (val === 'all') {
+      clearNoContactsMsg();
+      resetHiddenContactDivs();
+      return;
+    }
+
+
+    let contacts = document.querySelectorAll(".contact");
+    let noContactsElement = document.getElementById("no-contacts");
+
+    let matchExists = false;
+    contacts.forEach(contactDiv => {
+      let tagsElement = contactDiv.querySelector(".contact-tags");
+      if (!tagsElement) {
+        contactDiv.classList.add("hidden");
+        return;
+      }
+      let tags = tagsElement.textContent.split(",");
+      if (tags.includes(val)) {
+        matchExists = true;
+        clearNoContactsMsg();
+        contactDiv.classList.remove("hidden")
+      } else {
+        contactDiv.classList.add("hidden");
+      }
+    });
+
+    if (!matchExists && !noContactsElement) {
+      let html = `<h1 id="no-contacts">There are no matching contacts.</h1>`;
+      document.getElementById("contacts").insertAdjacentHTML("afterend", html);
+    }
   });
 });
 
@@ -93,12 +133,6 @@ async function deleteContact(id) {
   await fetch(`/api/contacts/${id}`, {method: "delete"}).catch(err => console.log("Delete failed."));
 }
 
-function parseTags(contactObj) {
-  let booleans = ["work", "friend", "family", "other"];
-  let tags = contactObj.tags.split(",");
-  booleans = booleans.filter(boolean => tags.includes(boolean));
-  return booleans.map(boolean => boolean + "Checked");
-}
 
 function formatTags(tagsArray) {
   let tags = '';
@@ -124,14 +158,20 @@ function addButtonListener(e) {
   clearNoContactsMsg();
   toggleHiddenDivs();
   clearSearchBar();
+  resetTagsFilter();
   let main = document.querySelector("main");
-  main.insertAdjacentHTML("afterbegin", formTemplate({action: "Add"}));
+  let templateObj = {
+    action: "Add",
+    additionalTags: addtlTags
+  }
+  main.insertAdjacentHTML("afterbegin", formTemplate(templateObj));
   bindFormButtons();
 }
 
 function bindFormButtons() {
   let submit = document.getElementById("submit");
   let cancel = document.getElementById("cancel");
+  let tagButton = document.getElementById("tag-button");
 
   cancel.addEventListener("click", e => {
     e.preventDefault();
@@ -142,7 +182,7 @@ function bindFormButtons() {
   submit.addEventListener("click", async e => {
     e.preventDefault();
     clearErrorMessages();
-    
+
     let success = true;
     if (document.getElementById("name").value.length === 0) {
       validateInput('name');
@@ -159,9 +199,26 @@ function bindFormButtons() {
     
     if (success) {
       submitContact();
+      addtlTags = await additionalTags();
       document.getElementById("contact-form").remove();
       await fetchHomepage();
       toggleHiddenDivs();
+    }
+  });
+
+  tagButton.addEventListener("click", e => {
+    e.preventDefault();
+    clearErrorMessages();
+
+    let text = document.getElementById("new-tag").value.toLowerCase();
+    if (text.length === 0 || text.includes(' ')) {
+      validateInput('tag-button');
+    } else {
+      let html = `<label class="tag" for="${text}">${text}</label>` +
+                `<input type="checkbox" id="${text}" name="${text}" checked/>`;
+      let tags = document.querySelectorAll("input[type='checkbox']");
+      let lastTag = tags[tags.length - 1];
+      lastTag.insertAdjacentHTML("afterend", html);
     }
   });
 }
@@ -193,12 +250,18 @@ async function submitContact() {
       method: 'put',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(data)
+    }).then(_json => {
+      additionalTags().then(val => addtlTags = val);
+      populateTagsFilter();
     }).catch(err => console.log("Put request unsuccessful"));
   } else { // Add new contact
     await fetch('/api/contacts', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(data)
+    }).then(_json => {
+      additionalTags().then(val => addtlTags = val);
+      populateTagsFilter();
     }).catch(err => console.log("Post request unsuccessful"));
   }
 }
@@ -227,13 +290,53 @@ function validateInput(elementId) {
 
   if (elementId === 'name' || elementId === 'phone') {
     msg = "Field cannot be left empty.";
-  } else {
+  } else if (elementId === 'email') {
     msg = "Please enter a valid email.";
-  }
+  } else msg = "Enter a valid tag. No spaces."
   element.insertAdjacentHTML("afterend", `<p class="input-error">${msg}</p>`);
 }
 
 function clearErrorMessages() {
   let errorMessages = document.querySelectorAll(".input-error");
   errorMessages.forEach(element => element.remove());
+}
+
+async function additionalTags() {
+  let defaultTags = ["work", "friend", "family", "other"];
+  let addtlTags = [];
+  await fetch("/api/contacts")
+  .then(response => response.json())
+  .then(json => {
+    let contacts = json.length > 0 ? json : null;
+    if (contacts) {
+      contacts.forEach(contact => {
+        if (!contact.tags) return;
+        let tags = contact.tags.split(",");
+        tags.forEach(tag => {
+          if (!defaultTags.includes(tag) && !addtlTags.includes(tag)) {
+            addtlTags.push({tag: tag, tagId: tag + 'Id'});
+          }
+        });
+      })
+    }
+  });
+  return addtlTags.length > 0 ? addtlTags : null;
+}
+
+async function populateTagsFilter() {
+  let addtlTags = await additionalTags();
+  let tags = ["work", "friend", "family", "other"];
+  if (addtlTags) tags = tags.concat(addtlTags.map(tag => tag.tag));
+  tags = tags.map(tag => {
+    return {tag};
+  });
+
+  let tagSelectElement = document.getElementById("tag-select");
+  tagSelectElement.innerHTML = '<option value="all">all</option>';
+  tagSelectElement.insertAdjacentHTML("beforeend", tagTemplate({tags}));
+}
+
+function resetTagsFilter() {
+  let tagSelectElement = document.getElementById("tag-select");
+  tagSelectElement.value = 'all';
 }
